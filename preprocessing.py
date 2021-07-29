ac244f22c18b6367ee5d64209cb3d1fde3129dfc
z-marina <8713530-zmarina@users.noreply.gitlab.com>
2021-07-29 10:22:09 +0100

    updated preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -15,130 +15,79 @@ import csv
 def preprocess_fertility(file_path):
     df = pd.read_csv(file_path, index_col=False)
     df = pd.melt(df, id_vars=["year"])
-    
-    childless = df[df.variable!="childless"]
+
+    childless = df[df.variable != "childless"]
     childless.to_csv("./data/childless.csv")
-    
-    df = df[(df.variable!="final") & (df.variable!="childless")].dropna().sort_values(by=["year","variable"], ascending=[False, True]).reset_index(drop=True)
-    
-    if (file_path=='./data/cohort.csv'):
+
+    df = (
+        df[(df.variable != "final") & (df.variable != "childless")]
+        .dropna()
+        .sort_values(by=["year", "variable"], ascending=[False, True])
+        .reset_index(drop=True)
+    )
+
+    if file_path == "./data/cohort.csv":
         df.value = df.value * 100
-    
+
     count_series = df.value_counts(["year"])
     count_df = count_series.to_frame("count").reset_index()
-    
+
     df = pd.merge(df, count_df, on="year")
-    df.to_csv(file_path[0:-4]+"_2d.csv", index=False)
+    df.to_csv(file_path[0:-4] + "_2d.csv", index=False)
 
 
 # In[31]:
 
 
-fertility_files = ['./data/cohort.csv', './data/cohort_total.csv']
+fertility_files = ["./data/cohort.csv", "./data/cohort_total.csv"]
 for file in fertility_files:
-    preprocess_fertility(file);
+    preprocess_fertility(file)
 
 
 # In[29]:
 
 
-fertility_files[0][0:-4]
+df = pd.read_csv("./data/numchildren_percentages.csv")
+
+# Cleanup
+df = df.fillna(method="ffill")
+df = df.dropna().reset_index(drop=True)
+df = df[df.age != "Final3"]
 
 
-# In[24]:
+df["zero"] = df["0"] / df["Total"]
+df["one"] = df["1"] / df["Total"]
+df["two"] = df["2"] / df["Total"]
+df["three"] = df["3"] / df["Total"]
+df["four"] = df["4"] / df["Total"]
+df = df[["year", "age", "zero", "one", "two", "three", "four"]]
 
 
-df2 = pd.read_csv(, index_col=False)
-df2 = pd.melt(df, id_vars=["year"])
-df2 = df[(df.variable!="final")].dropna().sort_values(by=["year","variable"], ascending=[False, True]).reset_index(drop=True)
-df2.to_csv("./data/cohort_total_2d.csv", index=False)
-max(df2.value)
+# In[271]:
 
 
-# In[23]:
+df[["year", "age", "zero", "one", "two", "three", "four"]].to_csv(
+    "./data/numchildren_percentages_normalised.csv", index=False
+)
+df3 = df[["year", "age", "one", "two", "three", "four"]]
+df3["Total"] = df3["one"] + df3["two"] + df3["three"] + df3["four"]
+df3["one"] = df3["one"] / df3["Total"]
+df3["two"] = df3["two"] / df3["Total"]
+df3["three"] = df3["three"] / df3["Total"]
+df3["four"] = df3["four"] / df3["Total"]
+df3["year"] = df3["year"] + pd.to_numeric(df3["age"], downcast="integer")
+df3[["year", "age", "one", "two", "three", "four"]].to_csv(
+    "./data/filtered_numchildren_percentages_normalised.csv", index=False
+)
 
 
+# In[288]:
 
 
-
-# In[89]:
-
-
-with open('./data/cohort.csv') as f:
-    reader = csv.reader(f)
-    rows = list(reader)
+df["year"] = df["year"] + pd.to_numeric(df["age"], downcast="integer")
 
 
-# In[90]:
+# In[289]:
 
 
-rows 
-
-
-# In[85]:
-
-
-with open('./json/cohort.json', 'w') as f:
-    json.dump(rows, f)
-
-
-# In[64]:
-
-
-rows
-
-
-# In[17]:
-
-
-import os
-
-
-# In[ ]:
-
-
-
-
-
-# In[43]:
-
-
-len(rows)
-
-
-# In[61]:
-
-
-file_list = os.listdir("./data/pyramid/")
-file_list.sort()
-
-
-# In[62]:
-
-
-
-pyramid = []
-for file in file_list:
-    with open(f'./data/pyramid/{file}') as f:
-        reader = csv.DictReader(f)
-        rows = list(reader)
-#         print(rows)
-        for row in rows:
-#             row.update({"Year":int(file[-8:-4])})
-            row["M"] = int(row["M"])
-            row["F"] = int(row["F"])
-        pyramid.append({"Year":int(file[0:4]), "Cohorts":rows})
-            
-print(pyramid)
-
-with open('./json/pyramid.json', 'w') as f:
-    json.dump(rows, f)
-
-
-# In[93]:
-
-
-df = pd.read_csv("./data/numchildren.csv").drop(columns="Total")
-df = pd.melt(df, id_vars=["mothersdob", "age"])
-df.to_csv("./data/numchildren.csv", index=False)
-
+pd.melt(df, id_vars=["year", "age"]).to_csv("./data/numchildren_2d.csv", index=False)
